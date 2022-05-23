2ca97c6a8ef7f6532ec34115f423c8632c0d1aff
GavinChou <gavingavinamd@gmail.com>
2022-05-23 12:09:39 +0800

    在 local master 「不」 Pull就修改4

--- fiveTest/Program.cs
+++ fiveTest/Program.cs
@@ -3,3 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("第一個 commit");
 Console.WriteLine("耳機在 local master 修改 1");
 Console.WriteLine("音響在 local master 修改 2");
+Console.WriteLine("音響在 local master 「不」 Pull就修改4");
